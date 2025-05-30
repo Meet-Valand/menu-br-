2bbbf319be6c181352c8b38f7dc906577f8fe3ab
Meet-Valand <meetvaland10096@gmail.com>
2025-05-30 11:09:54 +0530

    Final

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 // pages/index.tsx
 import HeaderCarousel from '@/components/HeadCarousel';
 import MenuSection from '@/components/MenuSection';
-
+import Footer from '@/components/Footer';
 export default function Home() {
   return (
     <main>
       <HeaderCarousel />
       <MenuSection />
+      <Footer />
     </main>
+
   );
 }
